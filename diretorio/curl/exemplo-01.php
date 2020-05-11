186eba47578d35d5559e070662d9936627ccab67
Pedro Ribeiro <pedrohenriqueribei@gmail.com>
2020-05-11 11:21:32 -0300

    TRABALHANDO COM CURL -> XML

---
+++ diretorio/curl/exemplo-01.php
@@ -0,0 +1,31 @@
+<?php 
+
+	$cep = "72318121";
+
+	$link = "https://viacep.com.br/ws/$cep/json/";
+
+	$ch = curl_init($link);
+
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+
+	$response = curl_exec($ch);
+
+	curl_close($ch);
+
+	$dados = json_decode($response, true);
+
+	print_r($dados);
+
+	$bairro = $dados["bairro"];
+
+
+echo "bairro: " . $bairro;
+
+
+
+
+	$logradouro = $dados["logradouro"];
+	echo "LOGRADOURO: " . $logradouro;
+ ?>
+
